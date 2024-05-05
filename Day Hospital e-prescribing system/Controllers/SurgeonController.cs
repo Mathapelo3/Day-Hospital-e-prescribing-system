fe6ff532de92adf6f3e3824cb800c82ffc25e861
Jade0106 <jadelangeveld@gmail.com>
2024-05-05 18:32:20 +0200

    changes made to surgeon controller

--- Day Hospital e-prescribing system/Controllers/SurgeonController.cs
+++ Day Hospital e-prescribing system/Controllers/SurgeonController.cs
@@ -8,5 +8,55 @@ namespace Day_Hospital_e_prescribing_system.Controllers
         {
             return View();
         }
+
+        public IActionResult Prescriptions()
+        {
+            return View();
+        }
+
+        public IActionResult Patients()
+        {
+            return View();
+        }
+
+        public IActionResult Surgeries()
+        {
+            return View();
+        }
+
+        public IActionResult NewPrescription()
+        {
+            return View();
+        }
+
+        public IActionResult EditPrescription()
+        {
+            return View();
+        }
+
+        public IActionResult NewSurgery()
+        {
+            return View();
+        }
+
+        public IActionResult BookedPatients()
+        {
+            return View();
+        }
+
+        public IActionResult AdmissionRecord()
+        {
+            return View();
+        }
+
+        public IActionResult PatientRecovery()
+        {
+            return View();
+        }
+
+        public IActionResult DischargePatient()
+        {
+            return View();
+        }
     }
 }
