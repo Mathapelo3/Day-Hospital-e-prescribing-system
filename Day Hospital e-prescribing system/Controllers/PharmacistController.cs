ef74aa736f04d157d4b238ce747c8e3708a3b2db
Mathapelo Mokgomong <mmmokgomong@gmail.com>
2024-05-05 20:54:01 +0200

    .

---
+++ Day Hospital e-prescribing system/Controllers/PharmacistController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Day_Hospital_e_prescribing_system.Controllers
+{
+    public class PharmacistController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult PharmacistDashboard()
+        {
+            string username = HttpContext.Session.GetString("UserName"); // Use HttpContext.Session
+
+            // Pass the username to the view
+            ViewBag.Username = username;
+            return View();
+        }
+
+        // Medicine
+        public IActionResult MedicineList()
+        {
+            return View();
+        }
+    }
+}
