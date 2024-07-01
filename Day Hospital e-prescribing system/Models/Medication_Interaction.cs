1aa8f2718e48f484494ac365660a755e99ebcedb
Ac90210 <s218190298@mandela.ac.za>
2024-07-01 22:47:11 +0200

    Medication_Interaction changes

--- Day Hospital e-prescribing system/Models/Medication_Interaction.cs
+++ Day Hospital e-prescribing system/Models/Medication_Interaction.cs
@@ -1,6 +1,17 @@
-﻿namespace Day_Hospital_e_prescribing_system.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Day_Hospital_e_prescribing_system.Models
 {
     public class Medication_Interaction
     {
+
+        [ForeignKey("ICD_10_Code")]
+        public int ICD_ID { get; set; }
+
+        [ForeignKey("Active_Ingredient")]
+        public int Active_IngredientID { get; set; }
+
+        public virtual ICDCodes? ICDCodes { get; set; }
+        public virtual Active_Ingredient? Active_Ingredient { get; set; }
     }
 }
