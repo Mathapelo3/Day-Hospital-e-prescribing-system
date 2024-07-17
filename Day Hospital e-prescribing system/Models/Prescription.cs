21e2d2909d082affecef00ded80814934bc881a0
Jade0106 <jadelangeveld@gmail.com>
2024-07-17 12:45:55 +0200

    updated prescription model

--- Day Hospital e-prescribing system/Models/Prescription.cs
+++ Day Hospital e-prescribing system/Models/Prescription.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Day_Hospital_e_prescribing_system.Models
 {
+    [Table("Prescription")]
     public class Prescription
     {
         [Key]
