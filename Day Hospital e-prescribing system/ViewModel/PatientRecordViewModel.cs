e1ecaa65c921f6d89f492f44a8bfa281e75d35e9
Jade0106 <jadelangeveld@gmail.com>
2024-08-30 18:02:42 +0200

    surgeon updated code added.

--- Day Hospital e-prescribing system/ViewModel/PatientRecordViewModel.cs
+++ Day Hospital e-prescribing system/ViewModel/PatientRecordViewModel.cs
@@ -4,16 +4,17 @@ namespace Day_Hospital_e_prescribing_system.ViewModel
 {
     public class PatientRecordViewModel
     {
-       
+        public string IDNo { get; set; }
         public Patient? Patient { get; set; }
-        public List<Allergy> Allergies { get; set; }
-        public List<Vitals> Vitals { get; set; }
+        public List<Allergy> Allergies { get; set; } = new List<Allergy>();
+        public List<Vitals> Vitals { get; set; } = new List<Vitals>();
+        public List<Condition> Conditions { get; set; } = new List<Condition>();
 
-        // Constructor to initialize lists
         public PatientRecordViewModel()
         {
             Allergies = new List<Allergy>();
             Vitals = new List<Vitals>();
+            Conditions = new List<Condition>();
         }
     }
 
