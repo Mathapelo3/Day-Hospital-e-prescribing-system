9bfac5674b7bd7277689ac6874298519a2afc0e5
Ac90210 <s218190298@mandela.ac.za>
2024-09-15 11:17:11 +0200

    PrescriptionVM added

--- Day Hospital e-prescribing system/ViewModel/PrescriptionVM.cs
+++ Day Hospital e-prescribing system/ViewModel/PrescriptionVM.cs
@@ -2,5 +2,18 @@
 {
     public class PrescriptionVM
     {
+        public int PrescriptionID { get; set; }
+        public string Instruction { get; set; }
+        public DateTime Date { get; set; }
+        public string Quantity { get; set; }
+        public string Status { get; set; }
+        public bool Urgency { get; set; }
+        public string MedicationName { get; set; }
+        public string SurgeonName { get; set; }
+        public string PatientName { get; set; }
+        public int SurgeonID { get; set; }
+        public int PatientID { get; set; }
+        public int MedicationID { get; set; }
+        public string Medication { get; set; }
     }
 }
