abe628176a2376e059d26c69cc7259c4373fa305
Jade0106 <jadelangeveld@gmail.com>
2024-10-13 12:07:23 +0200

    key error fixed

--- Day Hospital e-prescribing system/Models/DayHospitalMed_ActiveIngredients.cs
+++ Day Hospital e-prescribing system/Models/DayHospitalMed_ActiveIngredients.cs
@@ -1,7 +1,10 @@
-﻿namespace Day_Hospital_e_prescribing_system.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Day_Hospital_e_prescribing_system.Models
 {
     public class DayHospitalMed_ActiveIngredients
     {
+        [Key]
         public int DayHospitalMed_ActiveIngredientID {  get; set; }
 
         public int Active_IngredientID { get; set; }
