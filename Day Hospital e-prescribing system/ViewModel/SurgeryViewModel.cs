4aa4559821864a69a5f4c889a51fd3a325603d30
Anna33214 <aredlinghuys@gmail.com>
2024-10-16 08:36:13 +0200

    Changed surgery view model namespace

--- Day Hospital e-prescribing system/ViewModel/SurgeryViewModel.cs
+++ Day Hospital e-prescribing system/ViewModel/SurgeryViewModel.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Day_Hospital_e_prescribing_system.Models
+namespace Day_Hospital_e_prescribing_system.ViewModel
 {
     public class SurgeryViewModel
     {
